68238950bae0511b7f32cced0a1aa75de609ff3c
Promal <promal.barua.cse@gmail.com>
2021-12-25 16:21:30 +0600

    Initial Commit

---
+++ Half Sheet Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Half Sheet Demo
+//
+//  Created by Promal on 25/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Half Sheet Demo/Half_Sheet_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Half_Sheet_DemoApp.swift
+//  Half Sheet Demo
+//
+//  Created by Promal on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Half_Sheet_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
